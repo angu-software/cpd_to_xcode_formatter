44e2f138dd04f00f1600db93035a850e7d0b5791
angu-software <>
2025-05-23 21:33:23 +0200

    Start over with line formatting

--- Tests/XCFormatterTests/FormatterTests.swift
+++ Tests/XCFormatterTests/FormatterTests.swift
@@ -31,40 +31,6 @@ struct FormatterTests {
                 """)
         }
     }
-
-    @Test
-    func should_format_FileOccurrance_to_xcode() async throws {
-        let file = Duplication.FileOccurrance(filePath: "/some/path/source.swift", begin: 1, end: 2)
-
-        let output = formatter.format(file, otherFileLocation: "otherFileLocation")
-
-        #expect(output == """
-            /some/path/source.swift:1:0: warning: 📑 Line equal with otherFileLocation
-            /some/path/source.swift:2:0: warning: 📑 Line equal with otherFileLocation
-            """)
-    }
-
-    // MARK: - DuplicationFormatter
-
-    @Test
-    func should_format_eachLine() async throws {
-        let duplication = Duplication(lenght: 2, tokenCount: 3, fileOccurances: [
-            Duplication.FileOccurrance(filePath: "/some/path/source_1.swift", begin: 1, end: 3),
-            Duplication.FileOccurrance(filePath: "/some/path/source_2.swift", begin: 3, end: 5)
-        ])
-
-        let output = formatter.format(duplication)
-
-        #expect(output == """
-            /some/path/source_1.swift:1:0: warning: 📑 Line equal with source_2.swift:3:0
-            /some/path/source_1.swift:2:0: warning: 📑 Line equal with source_2.swift:4:0
-            /some/path/source_1.swift:3:0: warning: 📑 Line equal with source_2.swift:5:0
-            /some/path/source_2.swift:3:0: warning: 📑 Line equal with source_1.swift:1:0
-            /some/path/source_2.swift:4:0: warning: 📑 Line equal with source_1.swift:2:0
-            /some/path/source_2.swift:5:0: warning: 📑 Line equal with source_1.swift:3:0
-            """
-        )
-    }
 }
 
 // TODO: modes, start/end marker, warning for each line and each duplicate file
