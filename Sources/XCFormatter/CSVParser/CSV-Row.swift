2784bcea9b830600d42d89c7a0d012f4700f972b
angu-software <>
2025-05-23 12:06:25 +0200

    Add naive parsing implementation for CSV.Row

--- Sources/XCFormatter/CSVParser/CSV-Row.swift
+++ Sources/XCFormatter/CSVParser/CSV-Row.swift
@@ -5,11 +5,53 @@
 //  Created by Andreas Günther on 23.05.25.
 //
 
+import Foundation
+
 enum CSV {
     struct Row: Equatable {
         var lineCount: UInt
         var tokenCount: UInt
         var occurancesCount: UInt
         var occurances: [Occurance]
+    }
+}
+
+extension CSV.Row {
+
+    init(string: String) {
+        let lineComponents = string.components(separatedBy: ",")
+        let rawLineCount = lineComponents[0]
+        let rawTokenCount = lineComponents[1]
+        let rawOccurancesCount = lineComponents[2]
+
+        let rawOccurances = lineComponents.suffix(from: 3).map({ $0 })
+        var groupedArrays: [[String]] = []
+
+        for i in stride(from: 0, to: rawOccurances.count, by: 2) {
+            if i + 1 < rawOccurances.count {
+                groupedArrays.append([rawOccurances[i], rawOccurances[i + 1]])
+            }
+        }
+
+        let occurances = groupedArrays.map { rawOccuranceComponents in
+            let rawOccurance = rawOccuranceComponents.joined(separator: ",")
+            return CSV.Occurance(string: rawOccurance)
+        }
+
+        self.init(lineCount: NumberParser.parse(rawLineCount),
+                  tokenCount: NumberParser.parse(rawTokenCount),
+                  occurancesCount: NumberParser.parse(rawOccurancesCount),
+                  occurances: occurances)
+    }
+
+    private static func stringToNumber(_ source: String) -> UInt {
+        return UInt(source)!
+    }
+}
+
+struct NumberParser {
+
+    static func parse(_ string: String) -> UInt {
+        return UInt(string)!
     }
 }
---
+++ Tests/XCFormatterTests/CSVRowTests.swift
@@ -0,0 +1,36 @@
+//
+//  CSVRowTests.swift
+//  cpd_to_xcode_formatter
+//
+//  Created by Andreas Günther on 23.05.25.
+//
+
+import Foundation
+import Testing
+
+@testable import XCFormatter
+
+struct CSVRowTests {
+
+    private typealias Row = CSV.Row
+    private typealias Occurance = CSV.Occurance
+
+    @Test
+    func sould_parse_from_string() {
+        let source = "125,844,2,955,/Users/angu/Repos/pandocsios/Pandocs/Controller/DashboardController/DashboardViewController.swift,217,/Users/angu/Repos/pandocsios/Pandocs/Controller/ProfileController/ProfileViewController.swift"
+
+        let row = Row(string: source)
+
+        #expect(row == Row(lineCount: 125,
+                           tokenCount: 844,
+                           occurancesCount: 2,
+                           occurances: [
+                            .init(lineStart: 955,
+                                  filePath: "/Users/angu/Repos/pandocsios/Pandocs/Controller/DashboardController/DashboardViewController.swift"),
+                            .init(lineStart: 217,
+                                  filePath: "/Users/angu/Repos/pandocsios/Pandocs/Controller/ProfileController/ProfileViewController.swift")
+                           ]))
+    }
+
+    // TODO: not parse first line
+}
