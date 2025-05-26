06ab011274d0feeb1e5ee0e37b8bf1d71a74a0bb
angu-software <>
2025-05-26 11:32:07 +0200

    Add dependency to ArgumentParser

--- Package.swift
+++ Package.swift
@@ -6,10 +6,15 @@ import PackageDescription
 let package = Package(
     name: "cpd_to_xcode_formatter",
     platforms: [.macOS(.v13)],
+    dependencies: [
+        .package(url: "https://github.com/apple/swift-argument-parser.git", from: "1.5.0")
+    ],
     targets: [
         .executableTarget(
             name: "cpd_xc_format",
-            dependencies: [.target(name: "XCFormatter")]
+            dependencies: [.target(name: "XCFormatter"),
+                           .product(name: "ArgumentParser",
+                                    package: "swift-argument-parser")]
         ),
         .target(
             name: "XCFormatter"
